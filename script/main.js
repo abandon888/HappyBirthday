7440a9c16367c09d898b39fc9f9f9248188dd93c
Tsingper <tsingper@outlook.com>
2025-02-01 18:14:24 +0800

    fix: add start button click play

--- script/main.js
+++ script/main.js
@@ -29,30 +29,26 @@ const fetchData = () => {
     })
 }
 
-let audio = null
-
-// 在文档加载时预加载音频
-document.addEventListener("DOMContentLoaded", () => {
-  audio = new Audio("music/bgMusic.mp3")
-  audio.preload = "auto"
-})
+// 音乐播放
+const audio = new Audio("music/bgMusic.mp3")
+audio.preload = "auto"
 
 const playPauseButton = document.getElementById('playPauseButton')
-let isPlaying = false // 初始状态为未播放
+let isPlaying = false
+
+document.getElementById('startButton').addEventListener('click', () => {
+  togglePlay(true)
+})
 
 playPauseButton.addEventListener('click', () => {
-  isPlaying = !isPlaying // 切换播放状态
-
-  if (isPlaying) {
-    // 如果当前是播放状态，则开始播放音频并更新按钮样式
-    audio.play()
-    playPauseButton.classList.add('playing')
-  } else {
-    // 如果当前是暂停状态，则暂停音频并更新按钮样式
-    audio.pause()
-    playPauseButton.classList.remove('playing')
-  }
+  togglePlay(!isPlaying)
 })
+
+function togglePlay(play) {
+  isPlaying = play
+  play ? audio.play() : audio.pause()
+  playPauseButton.classList.toggle('playing', play)
+}
 
 
 
